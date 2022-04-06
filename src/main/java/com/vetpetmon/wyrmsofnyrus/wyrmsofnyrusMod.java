3bf0273f9f1b0b254e0560373747c91b356a8123
Vetpetmon <vetpetmon@gmail.com>
2022-04-06 19:47:09 -0400

    finally

--- src/main/java/com/vetpetmon/wyrmsofnyrus/wyrmsofnyrusMod.java
+++ src/main/java/com/vetpetmon/wyrmsofnyrus/wyrmsofnyrusMod.java
@@ -22,6 +22,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLInterModComms;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
@@ -32,6 +33,13 @@ import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.fml.common.registry.EntityEntry;
+import net.minecraftforge.fml.common.registry.EntityEntryBuilder;
+
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.EnumCreatureType;
 
 import net.minecraft.world.biome.Biome;
 import net.minecraft.init.Blocks;
@@ -40,6 +48,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.item.Item;
 import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
 
 import java.util.function.Supplier;
 
