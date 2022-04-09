735642d62bb36af2ce5b1518c3449a4a280d9d58
Vetpetmon <vetpetmon@gmail.com>
2022-04-09 09:28:26 -0400

    fix

--- src/main/java/com/vetpetmon/wyrmsofnyrus/client/renderer/entity/WyrmlingGeoRenderer.java
+++ src/main/java/com/vetpetmon/wyrmsofnyrus/client/renderer/entity/WyrmlingGeoRenderer.java
@@ -1,11 +1,11 @@
 package com.vetpetmon.wyrmsofnyrus.client.renderer.entity;
 
 import com.vetpetmon.wyrmsofnyrus.client.model.entity.WyrmlingModel;
-import com.vetpetmon.wyrmsofnyrus.entity.wyrms.EntityHexePod;
+import com.vetpetmon.wyrmsofnyrus.entity.wyrms.EntityWyrmling;
 import net.minecraft.client.renderer.entity.RenderManager;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
-public class WyrmlingGeoRenderer extends GeoEntityRenderer<EntityHexePod> {
+public class WyrmlingGeoRenderer extends GeoEntityRenderer<EntityWyrmling> {
     public WyrmlingGeoRenderer(RenderManager renderManager)
     {
         super(renderManager, new WyrmlingModel());
