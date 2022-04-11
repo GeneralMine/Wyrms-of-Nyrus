40fcf7d3dce5fc47f69dcc62076595dcb9ae073b
Vetpetmon <vetpetmon@gmail.com>
2022-04-10 22:34:13 -0400

    stop dying idiot

--- src/main/java/com/vetpetmon/wyrmsofnyrus/invasion/EntityDeathTimer.java
+++ src/main/java/com/vetpetmon/wyrmsofnyrus/invasion/EntityDeathTimer.java
@@ -11,9 +11,9 @@ public class EntityDeathTimer {
 	public static void executeProcedure(Map<String, Object> dependencies) {
 		Entity entity = (Entity) dependencies.get("entity");
 		double timer = 0;
-		while (((timer) <= 120)) {
+		while (((timer) <= 1200)) {
 			timer = (double) ((timer) + 1);
-			entity.attackEntityFrom(DamageSource.MAGIC, (float) 100000);
 		}
+		entity.attackEntityFrom(DamageSource.MAGIC, (float) 100000);
 	}
 }
