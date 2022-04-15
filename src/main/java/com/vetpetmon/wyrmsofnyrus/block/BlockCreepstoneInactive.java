50880ed079271df4246b0c41d6407639d5d6cf88
Vetpetmon <vetpetmon@gmail.com>
2022-04-14 23:33:37 -0400

    AaaqaAfdadsg

--- src/main/java/com/vetpetmon/wyrmsofnyrus/block/BlockCreepstoneInactive.java
+++ src/main/java/com/vetpetmon/wyrmsofnyrus/block/BlockCreepstoneInactive.java
@@ -15,9 +15,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.Item;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.material.MapColor;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.Block;
 
