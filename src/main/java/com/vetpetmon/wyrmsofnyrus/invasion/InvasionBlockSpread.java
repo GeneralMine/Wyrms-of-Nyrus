997220e5c929d00b1d03ede685d1b33dd29fba0e
Vetpetmon <vetpetmon@gmail.com>
2022-04-24 15:20:41 -0400

    Update InvasionBlockSpread.java

--- src/main/java/com/vetpetmon/wyrmsofnyrus/invasion/InvasionBlockSpread.java
+++ src/main/java/com/vetpetmon/wyrmsofnyrus/invasion/InvasionBlockSpread.java
@@ -8,10 +8,6 @@ import java.util.Map;
 
 public class InvasionBlockSpread {
     public static void run(Map<String, Object> dependencies) {
-        if (dependencies.get("world") == null) {
-            System.err.println("Failed to load dependency world for script InvasionBlockSpread!");
-            return;
-        }
         World world = (World) dependencies.get("world");
         wyrmVariables.WorldVariables
                 .get(world).wyrmInvasionPoints = (wyrmVariables.WorldVariables.get(world).wyrmInvasionPoints) + 0.015;
