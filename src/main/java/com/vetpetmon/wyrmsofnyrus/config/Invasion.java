e03a31f5b84cf6729a924fa074b9371d444c6a05
Vetpetmon <vetpetmon@gmail.com>
2022-05-09 21:43:02 -0400

    SIMPlify.

--- src/main/java/com/vetpetmon/wyrmsofnyrus/config/Invasion.java
+++ src/main/java/com/vetpetmon/wyrmsofnyrus/config/Invasion.java
@@ -1,8 +1,6 @@
 package com.vetpetmon.wyrmsofnyrus.config;
 
-import net.minecraft.block.Block;
-import net.minecraft.util.math.BlockPos;
-import net.minecraftforge.common.config.Config;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.config.Configuration;
 
 import java.util.ArrayList;
@@ -23,7 +21,7 @@ public class Invasion {
     public static float creepSpreadMaxHardness;
     public static String[] invalidBlocksForCreepspread;
     public static boolean CSBlockBLEnabled;
-    public static ArrayList<Block> iBdefFinal;
+    public static ArrayList<ResourceLocation> iBdefFinal;
     public static boolean creepSpreadsDiagonally;
 
     public static String[] iBdef = {"minecraft:furnace", "minecraft:brick_block", "minecraft:bone_block", "minecraft:bedrock", "minecraft:concrete", "minecraft:concrete_powder", "minecraft:end_bricks", "minecraft:end_stone", "minecraft:glass", "minecraft:jukebox", "minecraft:nether_brick", "minecraft:red_nether_brick", "minecraft:noteblock", "minecraft:observer", "minecraft:obsidian", "minecraft:packed_ice", "minecraft:prismarine", "minecraft:purpur_block", "minecraft:purpur_pillar", "minecraft:quartz_block", "minecraft:sponge", "minecraft:stained_glass", "minecraft:wool", "minecraft:stonebrick"};
@@ -46,7 +44,7 @@ public class Invasion {
         creepSpreadPoints = ConfigLib.createConfigDouble(config, CATEGORY, "Creep spread points", "Every time a creep block is created, the invasion points increase. If Invasion is not enabled, this won't work at all. It is recommended you should keep this number as a decimal unless if you want pain...", 0.015);
         creepSpreadMaxHardness = ConfigLib.createConfigDouble(config, CATEGORY, "Creep spread max hardness", "Maximum hardness of a block that can be infested. Can automatically generate a blacklist this way for other mods if you're too lazy to add to the blacklist below.", 2.45);
         invalidBlocksForCreepspread = ConfigLib.createConfigStringList(config, CATEGORY, "Creepable block blacklist", ("Blacklist of blocks hive creep can not spread to. Blocks that are not consider a full block by the MC engine do not need to be included here.\nWARNING: EXPERIMENTAL FEATURE. Disabled/enable in the 2nd config option. Is automatically set to true in development builds."), iBdef);
-        CSBlockBLEnabled = ConfigLib.createConfigBool(config, CATEGORY, "Creepable block blacklist", null, true);
+        CSBlockBLEnabled = ConfigLib.createConfigBool(config, CATEGORY, "Creepable block blacklist enabled", "Enables the creepable block blacklist.", true);
         FinalizeiBdef();
         creepSpreadsDiagonally = ConfigLib.createConfigBool(config, CATEGORY, "Creep spreads on diagonals", "Hive creep spreads to the 4 blocks directly touching it by default. This option, if set to true, will make it cover all blocks around it in a one-block radius. Disabled by default to minimize TPS impact.", false);
     }
@@ -64,9 +62,7 @@ public class Invasion {
     }
 
     public static void FinalizeiBdef() {
-        List iBdefF = Arrays.asList(iBdef);
-        iBdefFinal = new ArrayList<Block>(iBdefF);
-
-
+        List iBdefF = Arrays.asList(invalidBlocksForCreepspread);
+        iBdefFinal = new ArrayList<ResourceLocation>(iBdefF);
     }
 }
--- src/main/java/com/vetpetmon/wyrmsofnyrus/invasion/HiveCreepSpreadFurther.java
+++ src/main/java/com/vetpetmon/wyrmsofnyrus/invasion/HiveCreepSpreadFurther.java
@@ -1,9 +1,9 @@
 package com.vetpetmon.wyrmsofnyrus.invasion;
 
 import com.vetpetmon.wyrmsofnyrus.block.*;
-import com.vetpetmon.wyrmsofnyrus.config.ConfigLib;
 import com.vetpetmon.wyrmsofnyrus.config.Debug;
 import com.vetpetmon.wyrmsofnyrus.config.Invasion;
+import com.vetpetmon.wyrmsofnyrus.wyrmsofnyrus;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.world.World;
@@ -11,6 +11,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.block.material.Material;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class HiveCreepSpreadFurther{
 
@@ -48,7 +49,7 @@ public class HiveCreepSpreadFurther{
 		boolean isSoft = (world.getBlockState(i).getBlockHardness(world, i) < Invasion.creepSpreadMaxHardness);
 		boolean isFullCube = (world.getBlockState(i).isFullCube());
 		if ((Invasion.CSBlockBLEnabled) && !(i.equals(new BlockPos(x, y, z)))) {
-			boolean isUnAllowedBlock = ((Invasion.iBdefFinal.contains( (Block) ((world.getBlockState(i))))));
+			boolean isUnAllowedBlock = ((Invasion.iBdefFinal.contains((world.getBlockState(i).getBlock().getRegistryName()))));
 			return (!isAir) && (isSoft) && (isFullCube) && (!isUnAllowedBlock);
 		}
 		else {
@@ -65,21 +66,23 @@ public class HiveCreepSpreadFurther{
 			int y = pos.getY();
 			int z = pos.getZ();
 			BlockPosList = getCSPos(x,y,z);
+			BlockPos thisPos = BlockPosList.get(0);
+
 			if (world.isAirBlock(BlockPosList.get(2))) {
-				if (((world.getBlockState(BlockPosList.get(0))).getBlock() == Block.getBlockFromName("wyrmsofnyrus:hivecreepblock"))) {
-					world.setBlockState((BlockPosList.get(0)), BlockHiveCreepTop.block.getDefaultState(), 3);
+				if (((world.getBlockState(thisPos)).getBlock() == getCreepBlock("hivecreeptop"))) {
+					world.setBlockState((thisPos), BlockHiveCreepTop.block.getDefaultState(), 3);
 				}
 			}
 			for (BlockPos i : BlockPosList) {
 				if (creepspreadRules(i, world, x,y,z)) {
 					if (((world.getBlockState(i))).getBlock() == (Block.getBlockFromName("minecraft:glowstone"))) {
-						world.setBlockState((i), (IBlockState) Block.getBlockFromName("wyrmsofnyrus:wyrm_lights_yellow"), 3);
+						world.setBlockState(i, getCreepBlock("wyrm_lights_orange"), 3);
 						break;
-					} else if ((((world.getBlockState(i)).getMaterial() == Material.ROCK))) {
-						world.setBlockState((i), (IBlockState) Block.getBlockFromName("wyrmsofnyrus:creepstone"), 3);
+					} else if (matLookingBlock(i, Material.ROCK, world)) {
+						world.setBlockState(i, getCreepBlock("creepstone"), 3);
 						break;
-					} else if ((((world.getBlockState(i)).getMaterial() == Material.GROUND) || ((world.getBlockState(i)).getMaterial() == Material.GRASS))) {
-						world.setBlockState((i), (IBlockState) Block.getBlockFromName("wyrmsofnyrus:hivecreepblock"), 3);
+					} else if ((matLookingBlock(i, Material.GROUND, world) || (matLookingBlock(i, Material.GRASS, world)))) {
+						world.setBlockState(i, getCreepBlock("hivecreepblock"), 3);
 						break;
 					}
 				}
@@ -90,5 +93,8 @@ public class HiveCreepSpreadFurther{
 		if (Debug.LOGGINGENABLED && Debug.DEBUGLEVEL >= 5) System.out.println("Debugging: timespread for block at: " + (timesspread));
 		return timesspread;
 	}
+
+	public static IBlockState getCreepBlock(String block) {return (IBlockState) Block.getBlockFromName((wyrmsofnyrus.MODID + ":" + block));}
+	public static boolean matLookingBlock(BlockPos pos, Material mat, World world) {return (((world.getBlockState(pos)).getMaterial() == mat));}
 
 }
