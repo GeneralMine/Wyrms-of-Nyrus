4cf6d17a767f92ab426446202b8ac463259eeaf0
Vetpetmon <vetpetmon@gmail.com>
2022-05-14 10:34:55 -0400

    mmmmmm probers

---
+++ src/main/java/com/vetpetmon/wyrmsofnyrus/entity/ability/callouspodContents.java
@@ -0,0 +1,33 @@
+package com.vetpetmon.wyrmsofnyrus.entity.ability;
+
+import com.vetpetmon.wyrmsofnyrus.entity.wyrms.EntityCallousPod;
+import com.vetpetmon.wyrmsofnyrus.entity.wyrms.EntityDobber;
+import com.vetpetmon.wyrmsofnyrus.entity.wyrms.EntityWyrmProber;
+import com.vetpetmon.wyrmsofnyrus.synapselib.RNG;
+import net.minecraft.entity.Entity;
+import net.minecraft.world.World;
+
+import java.util.Map;
+
+public class callouspodContents {
+    public static void onDeath(Map<String, Object> dpnds) {
+        Entity entity = (Entity) dpnds.get("entity");
+        int x = (int) dpnds.get("x");
+        int y = (int) dpnds.get("y");
+        int z = (int) dpnds.get("z");
+        World world = (World) dpnds.get("world");
+        for (int index0 = 0; index0 < (RNG.getIntRangeInclu(1,2)); index0++) {
+            if ((entity instanceof EntityCallousPod)) {
+                if (!world.isRemote) {
+                    Entity entityToSpawn = new EntityWyrmProber(world);
+                    entityToSpawn.setLocationAndAngles(x, y, z, world.rand.nextFloat() * 360F, 0.0F);
+                    entityToSpawn = new EntityDobber(world);
+                    for (int i = 0; i < (2+(RNG.getIntRangeInclu(1,3))); i++) {
+                        entityToSpawn.setLocationAndAngles(x, y, z, world.rand.nextFloat() * 360F, 0.0F);
+                    }
+                    world.spawnEntity(entityToSpawn);
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/vetpetmon/wyrmsofnyrus/entity/wyrms/EntityCallousPod.java
@@ -0,0 +1,97 @@
+package com.vetpetmon.wyrmsofnyrus.entity.wyrms;
+
+import com.vetpetmon.wyrmsofnyrus.entity.EntityWyrm;
+import com.vetpetmon.wyrmsofnyrus.entity.ability.callouspodContents;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.entity.projectile.EntityPotion;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.world.World;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.PlayState;
+import software.bernie.geckolib3.core.builder.AnimationBuilder;
+import software.bernie.geckolib3.core.controller.AnimationController;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.manager.AnimationData;
+import software.bernie.geckolib3.core.manager.AnimationFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EntityCallousPod extends EntityWyrm implements IAnimatable{
+    private AnimationFactory factory = new AnimationFactory(this);
+    public EntityCallousPod(World world) {
+        super(world);
+        this.casteType = 9;
+        setSize(1f, 1f);
+        experienceValue = 0;
+        enablePersistence();
+        setNoAI(false);
+    }
+
+    @Override
+    public boolean attackEntityFrom(DamageSource source, float amount) {
+        if (source.getImmediateSource() instanceof EntityArrow)
+            return false;
+        if (source.getImmediateSource() instanceof EntityPlayer)
+            return false;
+        if (source.getImmediateSource() instanceof EntityPotion)
+            return false;
+        if (source == DamageSource.CACTUS)
+            return false;
+        if (source == DamageSource.LIGHTNING_BOLT)
+            return false;
+        return super.attackEntityFrom(source, amount);
+    }
+
+    @Override
+    protected void applyEntityAttributes() {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.ARMOR).setBaseValue(0.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.005D);
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(2D);
+        this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(1D);
+    }
+
+    @Override
+    public SoundEvent getAmbientSound() {
+        return null;
+    }
+
+    @Override
+    public SoundEvent getDeathSound() {
+        return SoundEvent.REGISTRY.getObject(new ResourceLocation("entity.enderdragon_fireball.explode"));
+    }
+
+    @Override
+    public void onDeath(DamageSource source) {
+        super.onDeath(source);
+        int x = (int) this.posX;
+        int y = (int) this.posY;
+        int z = (int) this.posZ;
+        Entity entity = this;
+        {
+            Map<String, Object> $_dmap = new HashMap<>();
+            $_dmap.put("entity", entity);
+            $_dmap.put("x", x);
+            $_dmap.put("y", y);
+            $_dmap.put("z", z);
+            $_dmap.put("world", world);
+            callouspodContents.onDeath($_dmap);
+        }
+    }
+
+    public void registerControllers(AnimationData data) {
+        data.addAnimationController(new AnimationController(this, "controller", 20F, this::predicate));
+    }
+
+    private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event)
+    {
+        event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.callouspod.living"));
+        return PlayState.CONTINUE;
+    }
+}
