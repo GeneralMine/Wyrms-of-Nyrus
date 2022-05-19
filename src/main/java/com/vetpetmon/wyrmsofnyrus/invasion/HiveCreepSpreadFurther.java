f49f70e67a8236a111cf0c023f59e0952fbe86e3
Vetpetmon <vetpetmon@gmail.com>
2022-05-19 15:27:28 -0400

    fallback

--- src/main/java/com/vetpetmon/wyrmsofnyrus/invasion/HiveCreepSpreadFurther.java
+++ src/main/java/com/vetpetmon/wyrmsofnyrus/invasion/HiveCreepSpreadFurther.java
@@ -70,36 +70,37 @@ public class HiveCreepSpreadFurther{
 				if (creepspreadRules(posi, world, x,y,z)) {
 					if (((world.getBlockState(posi))).getBlock() == (Block.getBlockFromName("minecraft:glowstone"))) {
 						world.setBlockState(posi, BlockWyrmLightsYellow.block.getDefaultState(), 3);
-						break;
 					} else if (matLookingBlock(posi, Material.ROCK, world)) {
 						world.setBlockState(posi, BlockCreepstone.block.getDefaultState(), 3);
-						break;
 					} else if ((matLookingBlock(posi, Material.GROUND, world) || (matLookingBlock(posi, Material.GRASS, world)))) {
 						world.setBlockState(posi, BlockHiveCreepBlock.block.getDefaultState(), 3);
-						break;
 					}
 				}
 			}
-			/*for (BlockPos i : BlockPosList) {
-
-				if (creepspreadRules(i, world, x,y,z)) {
-					if (((world.getBlockState(i))).getBlock() == (Block.getBlockFromName("minecraft:glowstone"))) {
-						world.setBlockState(i, BlockWyrmLightsYellow.block.getDefaultState(), 3);
-						break;
-					} else if (matLookingBlock(i, Material.ROCK, world)) {
-						world.setBlockState(i, BlockCreepstone.block.getDefaultState(), 3);
-						break;
-					} else if ((matLookingBlock(i, Material.GROUND, world) || (matLookingBlock(i, Material.GRASS, world)))) {
-						world.setBlockState(i, BlockHiveCreepBlock.block.getDefaultState(), 3);
-						break;
-					}
-				}
-			}*/
+			//failsafe(world, x, y, z); //Old code left for reference
 			BlockPosList.clear();
 			timesspread = ts + 1;
 		}
 		if (Debug.LOGGINGENABLED && Debug.DEBUGLEVEL >= 5) System.out.println("Debugging: timespread for block at: " + (timesspread));
 		return timesspread;
+	}
+
+	public static void failsafe(World world, int x, int y, int z) {
+		for (BlockPos i : BlockPosList) {
+
+			if (creepspreadRules(i, world, x,y,z)) {
+				if (((world.getBlockState(i))).getBlock() == (Block.getBlockFromName("minecraft:glowstone"))) {
+					world.setBlockState(i, BlockWyrmLightsYellow.block.getDefaultState(), 3);
+					break;
+				} else if (matLookingBlock(i, Material.ROCK, world)) {
+					world.setBlockState(i, BlockCreepstone.block.getDefaultState(), 3);
+					break;
+				} else if ((matLookingBlock(i, Material.GROUND, world) || (matLookingBlock(i, Material.GRASS, world)))) {
+					world.setBlockState(i, BlockHiveCreepBlock.block.getDefaultState(), 3);
+					break;
+				}
+			}
+		}
 	}
 
 	public static boolean matLookingBlock(BlockPos pos, Material mat, World world) {return (((world.getBlockState(pos)).getMaterial() == mat));}
