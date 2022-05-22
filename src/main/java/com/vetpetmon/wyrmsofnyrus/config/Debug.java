1ee3f74bee1bc82d07e70150e4b0b16741eabeed
Vetpetmon <vetpetmon@gmail.com>
2022-05-22 11:51:54 -0400

    Better configs

--- src/main/java/com/vetpetmon/wyrmsofnyrus/config/Debug.java
+++ src/main/java/com/vetpetmon/wyrmsofnyrus/config/Debug.java
@@ -4,8 +4,6 @@ import com.vetpetmon.wyrmsofnyrus.wyrmsofnyrus;
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.Configuration;
 
-import static com.vetpetmon.wyrmsofnyrus.config.ConfigLib.CFG_DIV;
-
 public class Debug {
     @Config.Comment("Enable or disable console logging")
     @Config.Name("Enable Debugging")
@@ -21,10 +19,10 @@ public class Debug {
 
         final String CATEGORY = "Debugging";
 
-        config.addCustomCategoryComment(CATEGORY, CFG_DIV + "\nMod dev & tester settings\n" + CFG_DIV);
+        config.addCustomCategoryComment(CATEGORY, "\nMod dev & tester settings\n");
 
-        LOGGINGENABLED = ConfigLib.createConfigBool(config, CATEGORY, "Enable logging", "Spams the console with various logs. If you enable this, make sure to edit the variable down below. \nDo not enable this unless if you're a developer, tester, or a modpack developer that needs to figure out what went wrong or why X thing isn't working. This is NOT intended for normal players to use at all.", false);
-        DEBUGLEVEL = ConfigLib.createConfigInt(config, CATEGORY, "Debug level", "The amount of console printing you want to destroy your hard drive with. Values above 10 won't do anything different. The larger this number, the more gets outputted. All increments of levels also include previous levels' loggings, so be careful about using this for extensive periods of times. \n- Level 1: minimal logging, will simply give a few warnings. \n- Level 2: Will output events like invasion event occurrences or invasion stage increments. \n- Level 3: Will output events like wyrmlings growing up \n- Level 4: Will output block mutations \n- Level 5: Will reveal block updates, entity deaths, etc. \n- Level 6: Major variable changes will be outputted into the log. \n- Level 7: All script variable changes will now output to the log, entity animation changes, and most sound plays are logged. \n- Level 8: EVERY METHOD CALL WILL BE OUTPUTTED INTO THE LOG. \n- Level 9: YOU BETTER HAVE A GOOD REASON WHY YOU WANT TO SEE EVERY SINGLE INPUT PUT INTO THOSE METHOD CALLS. \n- Level 10: I am not responsible if you brick your RAM or PC. You asked for it. Does everything in the previous levels, but also outputs more stuff like entity ticks and their NBT values, as well as every RNG roll, every... Thing. Turn back now, save your PC before it's too late.", 1);
+        LOGGINGENABLED = ConfigLib.createConfigBool(config, CATEGORY, "Enable logging", "Spams the console with various logs. If you enable this, make sure to edit the variable down below. \nDo not enable this unless if you're a developer, tester, or a modpack developer that needs to figure out what went wrong or why X thing isn't working. This is NOT intended for normal players to use at all. Default: false", false);
+        DEBUGLEVEL = ConfigLib.createConfigInt(config, CATEGORY, "Debug level", "The amount of console printing you want to destroy your hard drive with. Values above 10 won't do anything different. The larger this number, the more gets outputted. All increments of levels also include previous levels' loggings, so be careful about using this for extensive periods of times. \n- Level 1: minimal logging, will simply give a few warnings. \n- Level 2: Will output events like invasion event occurrences or invasion stage increments. \n- Level 3: Will output events like wyrmlings growing up \n- Level 4: Will output block mutations \n- Level 5: Will reveal block updates, entity deaths, etc. \n- Level 6: Major variable changes will be outputted into the log. \n- Level 7: All script variable changes will now output to the log, entity animation changes, and most sound plays are logged. \n- Level 8: EVERY METHOD CALL WILL BE OUTPUTTED INTO THE LOG. \n- Level 9: YOU BETTER HAVE A GOOD REASON WHY YOU WANT TO SEE EVERY SINGLE INPUT PUT INTO THOSE METHOD CALLS. \n- Level 10: I am not responsible if you brick your RAM or PC. You asked for it. Does everything in the previous levels, but also outputs more stuff like entity ticks and their NBT values, as well as every RNG roll, every... Thing. Turn back now, save your PC before it's too late. Default: 1", 1);
         if (DEBUGLEVEL > 10) DEBUGLEVEL = 10;
         else if (DEBUGLEVEL < 1) DEBUGLEVEL = 1;
         if (LOGGINGENABLED) wyrmsofnyrus.logger.info("Debug logging enabled! The current depth and level of debug logging is: " + DEBUGLEVEL);
--- src/main/java/com/vetpetmon/wyrmsofnyrus/config/Invasion.java
+++ src/main/java/com/vetpetmon/wyrmsofnyrus/config/Invasion.java
@@ -7,8 +7,6 @@ import net.minecraftforge.common.config.Configuration;
 
 import java.util.ArrayList;
 
-import static com.vetpetmon.wyrmsofnyrus.config.ConfigLib.CFG_DIV;
-
 public class Invasion {
     public static boolean invasionEnabled;
     public static boolean EXCANON;
@@ -30,27 +28,27 @@ public class Invasion {
     public static void loadFromConfig(Configuration config) {
 
         final String CATEGORY = "Invasion";
-        config.addCustomCategoryComment(CATEGORY, CFG_DIV + "\nThe Wyrm Invasion is the main mechanic of this mod, with a fully-fledged event system with threats that keep players on edge.\n" + CFG_DIV);
+        config.addCustomCategoryComment(CATEGORY,  "\nThe Wyrm Invasion is the main mechanic of this mod, with a fully-fledged event system with threats that keep players on edge.\n");
         config.setCategoryRequiresWorldRestart(CATEGORY, true);
 
-        invasionEnabled = ConfigLib.createConfigBool(config, CATEGORY, "Invasion enabled", "Enables the invasion system. Many functions of the mod will not work if this is off, including other sub-systems.", true);
+        invasionEnabled = ConfigLib.createConfigBool(config, CATEGORY, "Invasion enabled", "Enables the invasion system. Many functions of the mod will not work if this is off, including other sub-systems. Default: true", true);
         if (!invasionEnabled) wyrmsofnyrus.logger.info("Invasion module has been disabled");
 
-        EXCANON = ConfigLib.createConfigBool(config, CATEGORY, "SNAZ OS EX-tended canon", "Are we playing with the Bloodmind Nyral Wyrms, which bend their backs to take over ALL the worlds? (activates Nether and End invasions, makes wyrms immune to a LOT of damagetypes, and worsens invasion difficulty + hive creep.)\nThe EX-tended canon basically follows the Hostile Universe principle, where most, if not, all the aliens of the Stellar Networked Actuality Zone act as Great Filters to primitive or early space-age civilizations and worlds.\nEnabling this forces ALL features of this mod's invasion system with enhanced difficulty to exist.\nIf you are playing without other mods, you might want to be smart and keep this at false.\nIf you are playing with mods like SRP, HBM, TechGuns, Matter Overdrive, and/or Draconic evolution, you may want to set this to true.\nConfiguration options like EXCANON DIFFICULY will now apply.", false);
-        EXCANONDIFFICULTY = ConfigLib.createConfigDouble(config, CATEGORY, "EXCANON DIFFICULTY", "The factor of difficulty you wish to put yourself here. If for some reason that the doubled stats is not enough, you can increase this from the default 2.0x difficulty.", 2.0);
+        EXCANON = ConfigLib.createConfigBool(config, CATEGORY, "SNAZ OS EX-tended canon", "Are we playing with the Bloodmind Nyral Wyrms, which bend their backs to take over ALL the worlds? (activates Nether and End invasions, makes wyrms immune to a LOT of damagetypes, and worsens invasion difficulty + hive creep.)\nThe EX-tended canon basically follows the Hostile Universe principle, where most, if not, all the aliens of the Stellar Networked Actuality Zone act as Great Filters to primitive or early space-age civilizations and worlds.\nEnabling this forces ALL features of this mod's invasion system with enhanced difficulty to exist.\nIf you are playing without other mods, you might want to be smart and keep this at false.\nIf you are playing with mods like SRP, HBM, TechGuns, Matter Overdrive, and/or Draconic evolution, you may want to set this to true.\nConfiguration options like EXCANON DIFFICULY will now apply. Default: false", false);
+        EXCANONDIFFICULTY = ConfigLib.createConfigDouble(config, CATEGORY, "EXCANON DIFFICULTY", "The factor of difficulty you wish to put yourself here. If for some reason that the doubled stats is not enough, you can increase this from the default 2.0x difficulty. Default: 2.0", 2.0);
 
-        probingEnabled = ConfigLib.createConfigBool(config, CATEGORY, "Probing enabled", "Probers deal heavy damage and every kill advances the invasion by 2 points, compared to only one. They also have longer aggro range and fly faster. Set to false to disable this feature and make probers less dangerous.", true);
+        probingEnabled = ConfigLib.createConfigBool(config, CATEGORY, "Probing enabled", "Probers deal heavy damage and every kill advances the invasion by 2 points, compared to only one. They also have longer aggro range and fly faster. Set to false to disable this feature and make probers less dangerous. Default: true", true);
 
         creepTickRate = ConfigLib.createConfigInt(config, CATEGORY, "Creep tickrate", "Every n world ticks, hive creep blocks will tick and roll to see if they spread or not. 20 ticks = 1 second. Default: 400", 400);
-        creepEnabled = ConfigLib.createConfigBool(config, CATEGORY, "Creep enabled", "If hive creep is enabled or not. This stops spread, and also renders Creepwyrms useless (as they will not naturally spawn.)", true);
+        creepEnabled = ConfigLib.createConfigBool(config, CATEGORY, "Creep enabled", "If The Creep is enabled or not. This stops spread, and also renders Creepwyrms useless (Also disabling their spawning.) Default: true", true);
         if (!creepEnabled) wyrmsofnyrus.logger.info("Creep module has been disabled");
-        creepSpreadRate = ConfigLib.createConfigInt(config, CATEGORY, "Creep spread speed", "1 to n chance every tick that a hive creep blocks actually does something. Some blocks will tick much slower, like creepstone. Increase this number if you're seeing TPS drops", 10);
-        creepSpreadPoints = ConfigLib.createConfigDouble(config, CATEGORY, "Creep spread points", "Every time a creep block is created, the invasion points increase. If Invasion is not enabled, this won't work at all. It is recommended you should keep this number as a decimal unless if you want pain...", 0.015);
-        creepSpreadMaxHardness = ConfigLib.createConfigDouble(config, CATEGORY, "Creep spread max hardness", "Maximum hardness of a block that can be infested. Can automatically generate a blacklist this way for other mods if you're too lazy to add to the blacklist below.", 2.45);
+        creepSpreadRate = ConfigLib.createConfigInt(config, CATEGORY, "Creep spread speed", "1 to n chance every tick that a hive creep blocks actually does something. Some blocks will tick much slower, like creepstone. Increase this number if you're seeing TPS drops. Default: 10", 10);
+        creepSpreadPoints = ConfigLib.createConfigDouble(config, CATEGORY, "Creep spread points", "Every time a creep block is created, the invasion points increase. If Invasion is not enabled, this won't work at all. It is recommended you should keep this number as a decimal unless if you want pain... Default: 0.015", 0.015);
+        creepSpreadMaxHardness = ConfigLib.createConfigDouble(config, CATEGORY, "Creep spread max hardness", "Maximum hardness of a block that can be infested. Can automatically generate a blacklist this way for other mods if you're too lazy to add to the blacklist below. Default: 2.45", 2.45);
         invalidBlocksForCreepspread = ConfigLib.createConfigStringList(config, CATEGORY, "Creepable block blacklist", ("Blacklist of blocks hive creep can not spread to. Blocks that are not consider a full block by the MC engine do not need to be included here.\nWARNING: EXPERIMENTAL FEATURE. Disabled/enable in the 2nd config option. Is automatically set to true in development builds."), iBdef);
         CSBlockBLEnabled = ConfigLib.createConfigBool(config, CATEGORY, "Creepable block blacklist enabled", "Enables the creepable block blacklist.", true);
         FinalizeiBdef();
-        creepSpreadsDiagonally = ConfigLib.createConfigBool(config, CATEGORY, "Creep spreads on diagonals", "Hive creep spreads to the 4 blocks directly touching it by default. This option, if set to true, will make it cover all blocks around it in a one-block radius. Disabled by default to minimize TPS impact.", false);
+        creepSpreadsDiagonally = ConfigLib.createConfigBool(config, CATEGORY, "Creep spreads on diagonals", "Hive creep spreads to the 4 blocks directly touching it by default. This option, if set to true, will make it cover all blocks around it in a one-block radius. Disabled by default to minimize TPS impact. Default: false", false);
     }
 
     public static boolean isEXCANON() {
