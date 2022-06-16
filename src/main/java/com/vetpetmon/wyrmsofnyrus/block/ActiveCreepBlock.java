e11eb6e259c63290517092611cdf482ef64285ee
Vetpetmon <vetpetmon@gmail.com>
2022-06-16 12:33:09 -0400

    God dammit.
    Ok we're using the fallback system and it's enabled by default.
    
    You can opt-in to the experimental feature but I can't say for sure that 100% of the time it will work.
    
    Totally didn't go through my 9th Joker arc here.

--- src/main/java/com/vetpetmon/wyrmsofnyrus/block/ActiveCreepBlock.java
+++ src/main/java/com/vetpetmon/wyrmsofnyrus/block/ActiveCreepBlock.java
@@ -2,8 +2,10 @@ package com.vetpetmon.wyrmsofnyrus.block;
 
 
 import com.vetpetmon.wyrmsofnyrus.config.Debug;
+import com.vetpetmon.wyrmsofnyrus.config.Invasion;
 import com.vetpetmon.wyrmsofnyrus.synapselib.RNG;
 import com.vetpetmon.wyrmsofnyrus.synapselib.rangeCheck;
+import com.vetpetmon.wyrmsofnyrus.wyrmsofnyrus;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -12,18 +14,23 @@ public class ActiveCreepBlock {
 
     public static void CreepSpread(BlockPos pos, World world, IBlockState inactiveBlockName) {
         boolean doCheck = ((RNG.getIntRangeInclu(0,50)) >= 49);
-        if (doCheck) {
+        if (doCheck && Invasion.creepNewInactivity) {
             if (rangeCheck.material(world, pos, 4, BlockMaterials.CREEP, true)) {
                 if (Debug.LOGGINGENABLED && Debug.DEBUGLEVEL >= 8)
-                    System.out.println("Debugging: creep block at "
+                    wyrmsofnyrus.logger.info("Debugging: creep block at "
                             + pos
                             + " saw a non-creep block, so it can spread somewhere, thus, it was not turned inactive.");
             }
             else {
                 if (Debug.LOGGINGENABLED && Debug.DEBUGLEVEL >= 7)
-                    System.out.println("Debugging: creep block at " + pos + " was turned inactive");
+                    wyrmsofnyrus.logger.info("Debugging: creep block at " + pos + " was turned inactive");
                 world.setBlockState(pos, inactiveBlockName, 3);
             }
+        }
+        else if (doCheck){
+            if (Debug.LOGGINGENABLED && Debug.DEBUGLEVEL >= 7)
+                wyrmsofnyrus.logger.info("Debugging: creep block at " + pos + " was turned inactive");
+            world.setBlockState(pos, inactiveBlockName, 3);
         }
     }
 
--- src/main/java/com/vetpetmon/wyrmsofnyrus/config/Invasion.java
+++ src/main/java/com/vetpetmon/wyrmsofnyrus/config/Invasion.java
@@ -16,6 +16,7 @@ public class Invasion {
     public static int invasionPointsPerKill;
 
     public static boolean creepEnabled;
+    public static boolean creepNewInactivity;
     public static int creepSpreadRate;
     public static int creepTickRate;
     public static float creepSpreadPoints;
@@ -34,6 +35,10 @@ public class Invasion {
         config.addCustomCategoryComment(CATEGORY,  "\nThe Wyrm Invasion is the main mechanic of this mod, with a fully-fledged event system with threats that keep players on edge.\n");
         config.setCategoryRequiresWorldRestart(CATEGORY, true);
 
+        final String CATEGORYTWO = "Hive Creep";
+        config.addCustomCategoryComment(CATEGORYTWO,  "\nEverything involving Hive Creep\n");
+        config.setCategoryRequiresWorldRestart(CATEGORYTWO, true);
+
         invasionEnabled = ConfigLib.createConfigBool(config, CATEGORY, "Invasion enabled", "Enables the invasion system. Many functions of the mod will not work if this is off, including other sub-systems. Default: true", true);
         if (!invasionEnabled) wyrmsofnyrus.logger.info("Invasion module has been disabled");
 
@@ -44,14 +49,16 @@ public class Invasion {
 
         probingEnabled = ConfigLib.createConfigBool(config, CATEGORY, "Probing enabled", "Probers deal heavy damage and every kill advances the invasion by 5 points, compared to only one. They also have longer aggro range and fly faster. Set to false to disable this feature and make probers less dangerous. Default: true", true);
 
-        creepTickRate = ConfigLib.createConfigInt(config, CATEGORY, "Creep tickrate", "Every n world ticks, hive creep blocks will tick and roll to see if they spread or not. 20 ticks = 1 second. Default: 400", 400);
-        creepEnabled = ConfigLib.createConfigBool(config, CATEGORY, "Creep enabled", "If The Creep is enabled or not. This stops spread, and also renders Creepwyrms useless (Also disabling their spawning.) Default: true", true);
+        creepTickRate = ConfigLib.createConfigInt(config, CATEGORYTWO, "Creep tickrate", "Every n world ticks, hive creep blocks will tick and roll to see if they spread or not. 20 ticks = 1 second. Default: 400", 400);
+        creepNewInactivity = ConfigLib.createConfigBool(config, CATEGORYTWO, "Experimental creep inactivity algorithm", "By default, hive creep blocks just have a chance to turn inactive. The new algorithm checks all blocks in range for a non-creepable block. However, the new algorithm can be buggy and produce weird results, such as hive creep turning inactive ASAP, or it never turning inactive. Default: false", false);
+        creepEnabled = ConfigLib.createConfigBool(config, CATEGORYTWO, "Creep enabled", "If The Creep is enabled or not. This stops spread, and also renders Creepwyrms useless (Also disabling their spawning.) Default: true", true);
         if (!creepEnabled) wyrmsofnyrus.logger.info("Creep module has been disabled");
-        creepSpreadRate = ConfigLib.createConfigInt(config, CATEGORY, "Creep spread speed", "1 to n chance every tick that a hive creep blocks actually does something. Some blocks will tick much slower, like creepstone. Increase this number if you're seeing TPS drops. Default: 5", 5);
-        creepSpreadPoints = ConfigLib.createConfigDouble(config, CATEGORY, "Creep spread points", "Every time a creep block is created, the invasion points increase. If Invasion is not enabled, this won't work at all. It is recommended you should keep this number as a decimal unless if you want pain... Default: 0.015", 0.015);
-        creepSpreadMaxHardness = ConfigLib.createConfigDouble(config, CATEGORY, "Creep spread max hardness", "Maximum hardness of a block that can be infested. Can automatically generate a blacklist this way for other mods if you're too lazy to add to the blacklist below. Default: 2.45", 2.45);
-        invalidBlocksForCreepspread = ConfigLib.createConfigStringList(config, CATEGORY, "Creepable block blacklist", ("Blacklist of blocks hive creep can not spread to. Blocks that are not consider a full block by the MC engine do not need to be included here.\nWARNING: EXPERIMENTAL FEATURE. Disabled/enable in the 2nd config option. Is automatically set to true in development builds."), iBdef);
-        CSBlockBLEnabled = ConfigLib.createConfigBool(config, CATEGORY, "Creepable block blacklist enabled", "Enables the creepable block blacklist.", true);
+        if (creepNewInactivity) wyrmsofnyrus.logger.warn("We are using the new Creep inactivity algorithm. May the suns be on your sides. (This is EXPERIMENTAL)");
+        creepSpreadRate = ConfigLib.createConfigInt(config, CATEGORYTWO, "Creep spread speed", "1 to n chance every tick that a hive creep blocks actually does something. Some blocks will tick much slower, like creepstone. Increase this number if you're seeing TPS drops. Default: 5", 5);
+        creepSpreadPoints = ConfigLib.createConfigDouble(config, CATEGORYTWO, "Creep spread points", "Every time a creep block is created, the invasion points increase. If Invasion is not enabled, this won't work at all. It is recommended you should keep this number as a decimal unless if you want pain... Default: 0.015", 0.015);
+        creepSpreadMaxHardness = ConfigLib.createConfigDouble(config, CATEGORYTWO, "Creep spread max hardness", "Maximum hardness of a block that can be infested. Can automatically generate a blacklist this way for other mods if you're too lazy to add to the blacklist below. Default: 2.45", 2.45);
+        invalidBlocksForCreepspread = ConfigLib.createConfigStringList(config, CATEGORYTWO, "Creepable block blacklist", ("Blacklist of blocks hive creep can not spread to. Blocks that are not consider a full block by the MC engine do not need to be included here.\nWARNING: EXPERIMENTAL FEATURE. Disabled/enable in the 2nd config option. Is automatically set to true in development builds."), iBdef);
+        CSBlockBLEnabled = ConfigLib.createConfigBool(config, CATEGORYTWO, "Creepable block blacklist enabled", "Enables the creepable block blacklist.", true);
         FinalizeiBdef();
 
         invasionProgressionRate = ConfigLib.createConfigDouble(config, CATEGORY, "Invasion Progression Rate", "Speed up or slow down invasion stage progression by multiplying the point requirements. Default: 1.0", 1.0);
