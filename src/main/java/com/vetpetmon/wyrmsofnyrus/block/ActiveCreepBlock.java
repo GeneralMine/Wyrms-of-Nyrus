ad982368d2e6ecd2f6077671c975705889d420a0
Vetpetmon <vetpetmon@gmail.com>
2022-06-16 12:05:57 -0400

    ok maybe it's still busted. Trying something else.
    a million ways to code this YET NONE WORK.

--- src/main/java/com/vetpetmon/wyrmsofnyrus/block/ActiveCreepBlock.java
+++ src/main/java/com/vetpetmon/wyrmsofnyrus/block/ActiveCreepBlock.java
@@ -2,26 +2,22 @@ package com.vetpetmon.wyrmsofnyrus.block;
 
 
 import com.vetpetmon.wyrmsofnyrus.config.Debug;
-import com.vetpetmon.wyrmsofnyrus.config.Invasion;
-import com.vetpetmon.wyrmsofnyrus.invasion.HiveCreepSpreadFurther;
 import com.vetpetmon.wyrmsofnyrus.synapselib.RNG;
 import com.vetpetmon.wyrmsofnyrus.synapselib.rangeCheck;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-import java.util.ArrayList;
-
-import static com.vetpetmon.wyrmsofnyrus.invasion.HiveCreepSpreadFurther.creepspreadRules;
-
 public class ActiveCreepBlock {
 
     public static void CreepSpread(BlockPos pos, World world, IBlockState inactiveBlockName) {
-        boolean doCheck = ((RNG.getIntRangeInclu(0,50)) >= 43);
+        boolean doCheck = ((RNG.getIntRangeInclu(0,50)) >= 49);
         if (doCheck) {
-            if (!rangeCheck.material(world,pos,3,BlockMaterials.CREEP)) {
+            if (rangeCheck.material(world, pos, 4, BlockMaterials.CREEP, true)) {
                 if (Debug.LOGGINGENABLED && Debug.DEBUGLEVEL >= 8)
-                    System.out.println("Debugging: creep block at " + pos + " saw a non-creep block, so it can spread somewhere, thus, it was not turned inactive.");
+                    System.out.println("Debugging: creep block at "
+                            + pos
+                            + " saw a non-creep block, so it can spread somewhere, thus, it was not turned inactive.");
             }
             else {
                 if (Debug.LOGGINGENABLED && Debug.DEBUGLEVEL >= 7)
--- src/main/java/com/vetpetmon/wyrmsofnyrus/synapselib/rangeCheck.java
+++ src/main/java/com/vetpetmon/wyrmsofnyrus/synapselib/rangeCheck.java
@@ -41,10 +41,19 @@ public class rangeCheck {
             }
             sx = ((sx) + 1);
         }
-        if (found) return true;
-        else return false;
+        return found;
     }
-    public static boolean material(World world, BlockPos pos, double range, Material lookForMat) {
+    /**
+     * Checks a range of blocks for a specific block with a material in a cube area. Returns true if it's found the block, false if it didn't.
+     * Do not make this range huge. I'll actually cry if you do. Any big operation will cause TPS lag. Now with an exclusion check!
+     * @param world World
+     * @param pos Origin of the cube
+     * @param range Range of the cube
+     * @param lookForMat Block's material to test for
+     * @param exclusive Does it check to see if it's anything BUT the block it's looking for. Set to false by default
+     * @return Boolean value
+     */
+    public static boolean material(World world, BlockPos pos, double range, Material lookForMat, boolean exclusive) {
         int x = pos.getX();
         int y = pos.getY();
         int z = pos.getZ();
@@ -52,7 +61,6 @@ public class rangeCheck {
         double sx;
         double sy;
         double sz;
-        Material lookFor = lookForMat;
         sx = (-range/2);
         found =(false);
         for (int index0 = 0; index0 < (range); index0++) {
@@ -60,9 +68,12 @@ public class rangeCheck {
             for (int index1 = 0; index1 < range; index1++) {
                 sz =(-range/2);
                 for (int index2 = 0; index2 < range; index2++) {
-                    if ((world.getBlockState(new BlockPos((int) (x + (sx)), (int) (y + (sy)), (int) (z + (sz)))))
-                            .getMaterial() == lookFor
-                    ) {
+                    if (exclusive) {
+                        if (!((world.getBlockState(new BlockPos((int) (x + (sx)), (int) (y + (sy)), (int) (z + (sz))))).getMaterial() == lookForMat)) {
+                            found = true;
+                        }
+                    }
+                    else if ((world.getBlockState(new BlockPos((int) (x + (sx)), (int) (y + (sy)), (int) (z + (sz))))).getMaterial() == lookForMat) {
                         found = true;
                     }
                     sz = ((sz) + 1);
@@ -71,7 +82,10 @@ public class rangeCheck {
             }
             sx = ((sx) + 1);
         }
-        if (found) return true;
-        else return false;
+        return found;
+    }
+    public static boolean material(World world, BlockPos pos, double range, Material lookForMat)
+    {
+        return material(world, pos, range, lookForMat, false);
     }
 }
