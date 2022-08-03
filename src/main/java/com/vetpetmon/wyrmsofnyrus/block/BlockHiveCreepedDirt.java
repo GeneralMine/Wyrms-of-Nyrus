949186015f6487cb924beba740a2e05a57ffbe42
Vetpetmon <vetpetmon@gmail.com>
2022-08-03 13:18:28 -0400

    ok it works now.

--- src/main/java/com/vetpetmon/wyrmsofnyrus/block/BlockHiveCreepedDirt.java
+++ src/main/java/com/vetpetmon/wyrmsofnyrus/block/BlockHiveCreepedDirt.java
@@ -3,7 +3,6 @@ package com.vetpetmon.wyrmsofnyrus.block;
 
 import com.vetpetmon.wyrmsofnyrus.AutoReg;
 import com.vetpetmon.wyrmsofnyrus.creativetab.TabWyrms;
-import com.vetpetmon.wyrmsofnyrus.invasion.HiveCreepSpreadFurther;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.properties.PropertyInteger;
@@ -31,7 +30,7 @@ public class BlockHiveCreepedDirt extends AutoReg.ModElement {
 
 	@Override
 	public void initElements() {
-		elements.blocks.add(() -> new BlockCustom().setRegistryName("creepeddirt"));
+		elements.blocks.add(() -> new creepedDirt().setRegistryName("creepeddirt"));
 		elements.items.add(() -> new ItemBlock(block).setRegistryName(block.getRegistryName()));
 	}
 
@@ -41,9 +40,9 @@ public class BlockHiveCreepedDirt extends AutoReg.ModElement {
 		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0,
 				new ModelResourceLocation("wyrmsofnyrus:creepeddirt", "inventory"));
 	}
-	public static class BlockCustom extends creepStaged {
-		public static PropertyInteger STAGE = PropertyInteger.create("stage", 1, 3);
-		public BlockCustom() {
+	public static class creepedDirt extends creepStaged {
+
+		public creepedDirt() {
 			setUnlocalizedName("creepeddirt");
 			setSoundType(SoundType.GROUND);
 			setHarvestLevel("shovel", 1);
@@ -52,6 +51,16 @@ public class BlockHiveCreepedDirt extends AutoReg.ModElement {
 			setLightLevel(0F);
 			setLightOpacity(255);
 			setCreativeTab(TabWyrms.tab);
+		}
+		@Override
+		public void updateTick(World world, BlockPos pos, IBlockState state, Random random) {
+			super.updateTick(world, pos, state, random);
+			int stage = state.getValue(STAGE) + 1;
+			if (stage < 3) world.setBlockState(pos, state.withProperty(STAGE, stage), 2);
+			else if (stage >= 3) {
+				assert false;
+				convert(world, pos, BlockHiveCreepBlockInactive.block.getDefaultState());
+			}
 		}
 	}
 }
--- src/main/java/com/vetpetmon/wyrmsofnyrus/block/creepStaged.java
+++ src/main/java/com/vetpetmon/wyrmsofnyrus/block/creepStaged.java
@@ -1,8 +1,10 @@
 package com.vetpetmon.wyrmsofnyrus.block;
 
-import com.vetpetmon.wyrmsofnyrus.config.Invasion;
 import net.minecraft.block.Block;
+
+import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyInteger;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.ItemStack;
@@ -10,7 +12,6 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 import java.util.List;
-import java.util.Random;
 
 public abstract class creepStaged extends Block {
 
@@ -27,26 +28,29 @@ public abstract class creepStaged extends Block {
         list.add("Spreads!");
     }
 
+    public int getMetaFromState(IBlockState state)
+    {
+        return state.getValue(STAGE);
+    }
+    public BlockStateContainer createBlockState()
+    {
+        return new BlockStateContainer(this, new IProperty[] {STAGE});
+    }
+
     @Override
     public void onBlockAdded(World world, BlockPos pos, IBlockState state) {
         super.onBlockAdded(world, pos, state);
-        world.setBlockState(pos, state.withProperty(STAGE, 0), 2);
+        world.setBlockState(pos, state.withProperty(STAGE, 1), 2);
         world.scheduleUpdate(pos, this, this.tickRate(world));
-    }
-
-    public void updateTick(World world, BlockPos pos, IBlockState state, Random random) {
-        super.updateTick(world, pos, state, random);
-        int stage = state.getValue(STAGE);
-        if (stage <= 3) world.setBlockState(pos, state.withProperty(STAGE, stage++), 2);
-        if (stage >= 3) convert(world, pos);
     }
 
     /**
      * Converts a block into another block. OVERRIDE THIS IN EXTENDED CLASSES!
      * @param world World
      * @param pos BlockPos
+     * @param convertTo BlockState
      */
-    public void convert(World world, BlockPos pos) {
-        world.setBlockState(pos, BlockHiveCreepBlock.block.getDefaultState(), 3);
+    public void convert(World world, BlockPos pos, IBlockState convertTo) {
+        world.setBlockState(pos, convertTo, 3);
     }
 }
