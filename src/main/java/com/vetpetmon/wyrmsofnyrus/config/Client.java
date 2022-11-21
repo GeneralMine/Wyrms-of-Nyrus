c8a014340e290a0e0bec6ee65ffeb61f423a52a4
Iké <vetpetmon@hotmail.com>
2022-11-20 19:32:39 -0500

    Compact configs!

--- src/main/java/com/vetpetmon/wyrmsofnyrus/config/Client.java
+++ src/main/java/com/vetpetmon/wyrmsofnyrus/config/Client.java
@@ -9,7 +9,7 @@ import static com.vetpetmon.wyrmsofnyrus.synapselib.CFG.createConfigInt;
 
 public class Client {
     public static boolean fancyAnimations;
-
+    public static boolean compactConfig;
 
     public static void loadFromConfig(Configuration config) {
 
@@ -18,5 +18,7 @@ public class Client {
         config.addCustomCategoryComment(CATEGORY, "\nUser-end settings. Can be reloaded at any point in time.\n");
 
         fancyAnimations = createConfigBool(config, CATEGORY, "Enable fancy animations", "Allows for extra animations to be played for certain wyrms that have more than 2-3 animations, reduces the amount of conditionals being checked and might help with FPS on lower end PCs. Default: true", true);
+        compactConfig = createConfigBool(config, CATEGORY, "Compact Config", "If you want to use a simple, single-file configuration file, this will switch your configs from the wyrms folder to the general configs folder. Client settings not included.", false);
+
     }
 }
