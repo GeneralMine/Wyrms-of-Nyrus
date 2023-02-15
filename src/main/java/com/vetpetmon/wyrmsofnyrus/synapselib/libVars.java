8a6273bf9c31196c771cbabcd1fd60a29f3d0a1e
Iké <vetpetmon@hotmail.com>
2023-02-14 19:13:30 -0500

    Exiting beta status!

--- src/main/java/com/vetpetmon/wyrmsofnyrus/synapselib/libVars.java
+++ src/main/java/com/vetpetmon/wyrmsofnyrus/synapselib/libVars.java
@@ -5,7 +5,7 @@ import com.vetpetmon.wyrmsofnyrus.wyrmsofnyrus;
 public class libVars {
     public static final String ModID = "wyrmsofnyrus"; //System name (ID) for the mod.
     public static final String ModName = "Wyrms of Nyrus"; //User-friendly name for the mod.
-    public static final String ModVersion = "0.2.7"; //Version of the mod.
+    public static final String ModVersion = "0.3"; //Version of the mod.
     public static final String ExpectLibVersion = "1.0"; //Version of the library the mod needs/works best with.
     public static final Object modInstance = wyrmsofnyrus.instance; //Mod object (instance)
 }
